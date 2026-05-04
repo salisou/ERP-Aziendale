4d5e37b99fec8c8ebafe73a87d82749a0227e81e
Salisou <salisoumoussa1@gmail.com>
2026-05-04 11:25:39 +0200

    Aggiungere i file di progetto.

---
+++ ApsnetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApsnetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ApsnetCoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApsnetCoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
